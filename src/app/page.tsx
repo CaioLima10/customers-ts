601ec83f32f98b90db69876ca4885a494ff42953
Caio lima <caiohenrys50@gmail.com>
2024-01-02 16:44:43 -0300

    feat: create list customers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,124 @@
-import Image from 'next/image'
+"use client"
+
+import { api } from "@/services/api";
+import { FormEvent, useEffect, useRef, useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
+
+interface ICustomersProps{
+  name: string
+  email: string
+  id: string
+  status: boolean
+  created_at: string
+}
 
 export default function Home() {
+
+  const [ customers , setCustomers ] = useState<ICustomersProps[]>([])
+
+  const nameRef = useRef<HTMLInputElement | null>(null)
+  const emailRef = useRef<HTMLInputElement | null>(null)
+
+  useEffect(() =>{
+    loadCustomers()
+  },[])
+
+  const loadCustomers = async () => {
+
+    try {
+      const response = await api.get("/customers")
+      setCustomers(response.data)
+      
+    } catch (error) {
+      throw new Error("nenhum dado encontrado")
+    }
+  }
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault()
+
+    if(!nameRef.current?.value || !emailRef.current?.value) return
+
+    const response = await api.post("/customer", {
+      name: nameRef.current?.value,
+      email: emailRef.current?.value
+    })
+
+    setCustomers(allCustomers => [...allCustomers , response.data])
+
+    nameRef.current.value = ''
+    emailRef.current.value = ''
+  }
+
+  const handleDelete = async (id: string) => {
+    try {
+      await api.delete("/customer", {
+        params: {
+          id: id
+        }
+      })
+      
+      const allCustomers = customers.filter((customer) => customer.id !== id)
+
+      setCustomers(allCustomers)
+
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main className="flex min-h-screen justify-center gray-900 px-4">
+      <div className='flex flex-col w-full my-10 md:max-w-2xl'>
+        <h1 className='text-4xl text-zinc-100 font-medium'>Clientes</h1>
+
+        <form className='flex flex-col my-6' onSubmit={handleSubmit}>
+          <label className='font-medium text-zinc-100'>Nome:</label>
+          <input 
+            type="text"
+            placeholder='Digite seu Nome completo...'
+            className='w-full mb-5 p-2 rounded-md text-zinc-900'
+          />
+
+          <label className='font-medium text-zinc-100'>Email:</label>
+          <input 
+            type="email"
+            placeholder='Digite seu Email...'
+            className='w-full mb-5 p-2 rounded-md text-zinc-900'
+            ref={nameRef}
+          />
+
+          <input 
+            type="submit" 
+            value="Cadastrar"
+            className='cursor-pointer w-full p-2 bg-green-500 rounded-md font-medium'
+            ref={emailRef}
+          />
+
+        </form>
+      
+        { customers.map((customer) => (
+          <section key={customer.id} className='flex flex-col relative hover:scale-105 duration-200 mb-4'>        
+                <article className='w-full bg-zinc-100 text-zinc-900 rounded-md p-2'>
+                  <p>Nome:<span className='font-medium '>{customer.name}</span></p>
+                  <p>Email:<span className='font-medium'>{customer.email}</span></p>
+                  <p>Status:<span className='font-medium'>{customer.status ? 'ATIVO' : 'INATIVO'}</span></p>
+
+                  <button 
+                    onClick={() => handleDelete(customer.id)}
+                    className="bg-red-500 w-7 h-7 flex items-center 
+                    justify-center rounded-md absolute right-2 -top-3"  
+                  >
+                    <FaTrashAlt size={18} color="#FFF" />
+                  </button>
+
+                </article>
+          </section>
+        )) }
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
